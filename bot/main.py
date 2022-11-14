df5d010dd0a5594358d5fa07757e1ea2b202b951
NovaXe <Jrwake04@gmail.com>
2022-11-14 13:54:57 -0500

    implemented test command

--- bot/main.py
+++ bot/main.py
@@ -5,26 +5,28 @@ import discord.utils
 import asyncio
 from discord.ext import commands
 
-with open("data/config.json") as f:
-    config = json.load(f)
-
 
 class Kagami(commands.Bot):
     def __init__(self):
-        super(Kagami, self).__init__(command_prefix=config["prefix"], intents=discord.Intents().all())
+        with open("bot/data/config.json") as f:
+            self.config = json.load(f)
+        super(Kagami, self).__init__(command_prefix=self.config["prefix"], intents=discord.Intents().all())
+
+    def start_bot(self):
+        self.run(self.config["token"])
 
     async def setup_hook(self):
-        for file in os.listdir("cogs"):
+        for file in os.listdir("bot/cogs"):
             if file.endswith(".py"):
                 name = file[:-3]
                 await self.load_extension(f"cogs.{name}")
-        await bot.tree.sync()
+        # await bot.tree.sync()
 
     async def on_ready(self):
         print(f"Logged in as {self.user} (ID: {self.user.id})")
 
 
 if __name__ == '__main__':
-    bot = Kagami()
-    bot.run(config["token"])
+    kagami = Kagami()
+    kagami.start_bot()
 
