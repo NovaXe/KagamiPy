aa066018aac634d92c9013cdc0d51e50893ee14e
NovaXe <Jrwake04@gmail.com>
2022-11-14 12:22:24 -0500

    Began implementing module importing

--- cogs/testCog.py
+++ cogs/testCog.py
@@ -9,3 +9,8 @@ class TestCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = bot.config
+
+    @commands.hybrid_command()
+    async def echo(self, ctx, *args):
+        await ctx.send(args)
+
--- main.py
+++ main.py
@@ -2,20 +2,26 @@ import json
 import os
 import discord
 import discord.utils
+import asyncio
 from discord.ext import commands
 
 with open("data/config.json") as f:
     config = json.load(f)
 
 
-class Kagami(commands.bot):
+class Kagami(commands.Bot):
     def __init__(self):
         super(Kagami, self).__init__(command_prefix=config["prefix"], intents=discord.Intents().all())
 
+    async def setup_hook(self):
         for file in os.listdir("cogs"):
             if file.endswith(".py"):
                 name = file[:-3]
-                self.load_extension(f"cogs.{name}")
+                await self.load_extension(f"cogs.{name}")
+        await bot.tree.sync()
+
+    async def on_ready(self):
+        print(f"Logged in as {self.user} (ID: {self.user.id})")
 
 
 if __name__ == '__main__':
