68c8adab3edd2d0e587aeac1dc0f7b1583ef732d
NovaXe <Jrwake04@gmail.com>
2023-03-23 19:53:06 -0400

    Added loose tag search and a way to list all tags on a server or globally

--- bot/cogs/tags.py
+++ bot/cogs/tags.py
@@ -1,22 +1,31 @@
+import datetime
+
 import discord
 from discord import app_commands
 from discord.ext import commands
 from bot.utils.bot_data import Server
+from bot.utils.ui import MessageScroller
 
 from typing import Literal
-
-
+from bot.kagami import Kagami
+from datetime import date
+from difflib import (
+    get_close_matches,
+    SequenceMatcher
+)
 
 
 class Tags(commands.GroupCog, group_name="tag"):
     def __init__(self, bot):
-        self.bot = bot
+        self.bot: Kagami = bot
         self.config = bot.config
 
 
-    get = app_commands.Group(name="get", description="gets a tag")
-    set = app_commands.Group(name="set", description="sets a tag")
-    delete = app_commands.Group(name="delete", description="deletes a tag")
+    get_group = app_commands.Group(name="get", description="gets a tag")
+    set_group = app_commands.Group(name="set", description="sets a tag")
+    delete_group = app_commands.Group(name="delete", description="deletes a tag")
+    list_group = app_commands.Group(name="list", description="lists the tags")
+    search_group = app_commands.Group(name="search", description="searches for tags")
 
     async def server_autocomplete(self, interaction: discord.Interaction, current: str) -> list[app_commands.Choice[str]]:
         user = interaction.user
@@ -49,7 +58,7 @@ class Tags(commands.GroupCog, group_name="tag"):
             # print("server tag")
 
             # print(interaction.namespace["server"])
-            # guild = discord.utils.get(interaction.user.mutual_guilds, name=interaction.namespace["server"])
+            # guild = discord.utils.get(interaction.user.mutual_guilds, name=interaction.namespace["server_id"])
             server_id = interaction.namespace["server"]
             if server_id:
                 tags = self.bot.fetch_server(server_id).tags
@@ -60,15 +69,60 @@ class Tags(commands.GroupCog, group_name="tag"):
             for tag_name, tag_data in tags.items() if current.lower() in tag_name.lower()
         ]
 
-    @app_commands.command(name="search", description="search for a tag")
-    async def _search(self, interaction: discord.Interaction, search: str):
-        await interaction.response.defer(thinking=True)
 
-        await interaction.edit_original_response(content="lol haven't added this yet")
+    # Was going to write my own but snagged this baby off stack overflow lol
+    @staticmethod
+    def find_closely_matching_tags(search: str, tags: dict, n, cutoff=0.45):
+        input_list = tags.items()
+        matches = list()
+        for key, value in input_list:
+            if len(matches) > n:
+                break
+            if SequenceMatcher(None, search, key).ratio() >= cutoff:
+                matches.append([key, value])
+        return dict(matches)
+
+
+    @search_group.command(name="global", description="searches for a global tag")
+    async def search_global(self, interaction: discord.Interaction, search: str, count: int=10):
+        await interaction.response.defer(thinking=True)
+        matches = self.find_closely_matching_tags(search, self.bot.global_tags, count)
+
+        pages = self.create_tag_pages(source="global", tags=matches, is_search=True)
+        message = await(await interaction.edit_original_response(content=pages[0])).fetch()
+        if count > 10:
+            view = MessageScroller(message=message, pages=pages, home_page=0, timeout=300)
+            await interaction.edit_original_response(content=pages[0], view=view)
+
+    @search_group.command(name="local", description="searches for a tag on this server_id")
+    async def search_local(self, interaction: discord.Interaction, search: str, count: int = 10):
+        await interaction.response.defer(thinking=True)
+        server: Server = self.bot.fetch_server(interaction.guild_id)
+        matches = self.find_closely_matching_tags(search, server.tags, count)
+        pages = self.create_tag_pages(source=interaction.guild.name, tags=matches, is_search=True)
+        message = await(await interaction.edit_original_response(content=pages[0])).fetch()
+        if count > 10:
+            view = MessageScroller(message=message, pages=pages, home_page=0, timeout=300)
+            await interaction.edit_original_response(content=pages[0], view=view)
+
+    @app_commands.autocomplete(server=server_autocomplete)
+    @search_group.command(name="server", description="searches for a tag on another server_id")
+    async def search_server(self, interaction: discord.Interaction, server: str, search: str, count: int=10):
+        await interaction.response.defer(thinking=True)
+        guild_name = discord.utils.get(self.bot.guilds, id=int(server)).name
+        server: Server = self.bot.fetch_server(server)
+        matches = self.find_closely_matching_tags(search, server.tags, count)
+        pages = self.create_tag_pages(source=guild_name, tags=matches, is_search=True)
+        message = await(await interaction.edit_original_response(content=pages[0])).fetch()
+        if count > 10:
+            view = MessageScroller(message=message, pages=pages, home_page=0, timeout=300)
+            await interaction.edit_original_response(content=pages[0], view=view)
+
+
 
 
     @app_commands.autocomplete(tag_name=tag_autocomplete)
-    @get.command(name="global", description="get a global tag")
+    @get_group.command(name="global", description="get a global tag")
     async def get_global(self, interaction: discord.Interaction, tag_name: str):
         await interaction.response.defer(thinking=True)
         # if not self.bot.global_tags:
@@ -81,7 +135,7 @@ class Tags(commands.GroupCog, group_name="tag"):
         await interaction.edit_original_response(content=tag_data["content"])
 
     @app_commands.autocomplete(tag_name=tag_autocomplete)
-    @get.command(name="local", description="get a tag from the server")
+    @get_group.command(name="local", description="get a tag from the server")
     async def get_local(self, interaction: discord.Interaction, tag_name: str):
         await interaction.response.defer(thinking=True)
         server: Server = self.bot.fetch_server(interaction.guild_id)
@@ -92,7 +146,7 @@ class Tags(commands.GroupCog, group_name="tag"):
         await interaction.edit_original_response(content=tag_data["content"])
 
     @app_commands.autocomplete(server=server_autocomplete, tag_name=tag_autocomplete)
-    @get.command(name="server", description="get a tag from another server")
+    @get_group.command(name="server", description="get a tag from another server")
     async def get_server(self, interaction: discord.Interaction, server: str, tag_name: str):
         await interaction.response.defer(thinking=True)
         server: Server = self.bot.fetch_server(server)
@@ -104,12 +158,7 @@ class Tags(commands.GroupCog, group_name="tag"):
         await interaction.edit_original_response(content=tag_data["content"])
 
 
-    # @app_commands.command(name="set", description="set a new tag")
-    # async def _set(self, interaction: discord.Interaction, tag_name: str, content: str):
-    #     pass
-
-
-    @set.command(name="local", description="set a tag for this server")
+    @set_group.command(name="local", description="set a tag for this server")
     async def set_local(self, interaction: discord.Interaction, tag_name: str, content: str):
         await interaction.response.defer(thinking=True)
         server: Server = self.bot.fetch_server(interaction.guild_id)
@@ -118,13 +167,15 @@ class Tags(commands.GroupCog, group_name="tag"):
             return
         server.tags.update({
             tag_name: {
-                "content": content
+                "content": content,
+                "author": interaction.user.name,
+                "creation_date": date.today().strftime("%m/%d/%y")
             }})
 
         await interaction.edit_original_response(content=f"Added tag **`{tag_name}`** to the server")
 
 
-    @set.command(name="global", description="set a global tag")
+    @set_group.command(name="global", description="set a global tag")
     async def set_global(self, interaction: discord.Interaction, tag_name: str, content: str):
         await interaction.response.defer(thinking=True)
         if tag_name in self.bot.global_tags.keys():
@@ -138,10 +189,8 @@ class Tags(commands.GroupCog, group_name="tag"):
         await interaction.edit_original_response(content=f"Added tag **`{tag_name}`** to globals")
         pass
 
-
-
     @app_commands.autocomplete(tag_name=tag_autocomplete)
-    @delete.command(name="local", description="Deletes a tag from this server")
+    @delete_group.command(name="local", description="Deletes a tag from this server")
     async def delete_local(self, interaction: discord.Interaction, tag_name: str):
         await interaction.response.defer(thinking=True)
         server: Server = self.bot.fetch_server(interaction.guild_id)
@@ -153,7 +202,7 @@ class Tags(commands.GroupCog, group_name="tag"):
         await interaction.edit_original_response(content=f"The tag **`{tag_name}`** has been deleted")
 
     @app_commands.autocomplete(tag_name=tag_autocomplete)
-    @delete.command(name="global", description="Deletes a global tag")
+    @delete_group.command(name="global", description="Deletes a global tag")
     async def delete_global(self, interaction: discord.Interaction, tag_name: str):
         await interaction.response.defer(thinking=True)
         if tag_name not in self.bot.global_tags.keys():
@@ -161,6 +210,73 @@ class Tags(commands.GroupCog, group_name="tag"):
             return
         self.bot.global_tags.pop(tag_name, None)
         await interaction.edit_original_response(content=f"The global tag **`{tag_name}`** has been deleted")
+
+    @list_group.command(name="global", description="lists the global tags")
+    async def list_global(self, interaction: discord.Interaction):
+        await interaction.response.defer(thinking=True)
+        pages = self.create_tag_pages('global', self.bot.global_tags)
+        message = await(await interaction.edit_original_response(content=pages[0])).fetch()
+        view = MessageScroller(message=message, pages=pages, home_page=0, timeout=300)
+        await interaction.edit_original_response(content=pages[0], view=view)
+
+
+    @list_group.command(name="local", description="lists this server's tags")
+    async def list_local(self, interaction: discord.Interaction):
+        await interaction.response.defer(thinking=True)
+        server: Server = self.bot.fetch_server(interaction.guild_id)
+        pages = self.create_tag_pages(interaction.guild.name, server.tags)
+        message = await(await interaction.edit_original_response(content=pages[0])).fetch()
+        view = MessageScroller(message=message, pages=pages, home_page=0, timeout=300)
+        await interaction.edit_original_response(content=pages[0], view=view)
+
+    @app_commands.autocomplete(server=server_autocomplete)
+    @list_group.command(name="server", description="lists another server's tags")
+    async def list_server(self, interaction: discord.Interaction, server: str):
+        await interaction.response.defer(thinking=True)
+        guild_name = discord.utils.get(self.bot.guilds, id=int(server)).name
+        server: Server = self.bot.fetch_server(server)
+        pages = self.create_tag_pages(guild_name, server.tags)
+        message = await(await interaction.edit_original_response(content=pages[0])).fetch()
+        view = MessageScroller(message=message, pages=pages, home_page=0, timeout=300)
+        await interaction.edit_original_response(content=pages[0], view=view)
+
+
+    @staticmethod
+    def create_tag_pages(source: str, tags: dict, is_search=False):
+        tag_count = len(tags)
+        num_full_pages, last_page_elem_count = divmod(tag_count, 10)
+        page_count = num_full_pages + 1 if last_page_elem_count else 0
+        pages = [""] * page_count
+        info_text = f"```swift\n{'Kagami' if source=='global' else source} has {tag_count}{' global' if source=='global' else ''} tags registered\n"
+        if is_search:
+            info_text = f"```swift\nFound {tag_count}{' global' if source == 'global' else ''} tags that are similar to your search {f'on {source}' if source !='global' else ''}\n"
+        else:
+            info_text = f"```swift\n{'Kagami' if source == 'global' else source} has {tag_count}{' global' if source == 'global' else ''} tags registered\n"
+
+        page_index = 0
+        elem_count = 0
+        for tag_name, tag_data in sorted(tags.items()):
+            if len(tag_name) <= 20:
+                new_name = tag_name.ljust(20)
+            else:
+                new_name = (tag_name[:16] + " ...").ljust(20)
+
+            creation_date = tag_data['creation_date'] if 'creation_date' in tag_data else '##/##/##'
+            tag_author = tag_data['author'] if 'author' in tag_data else 'Unknown'
+            content = f"{f'{page_index*10 + elem_count+1})'.ljust(4)}{new_name} - Created: {creation_date}  By: {tag_author}\n"
+            pages[page_index] += content
+
+
+
+
+            elem_count += 1
+            if elem_count == 10 or (page_index + 1 == page_count and elem_count == last_page_elem_count):
+                pages[page_index] = info_text + pages[page_index] + f"Page #: {page_index+1} / {page_count}\n```"
+                page_index = 1
+                elem_count = 0
+        if not pages:
+            pages.append(info_text + "\n```")
+        return pages
 
 
 
---
+++ bot/kagami.py
@@ -0,0 +1,154 @@
+import json
+import os
+import discord
+import discord.utils
+import asyncio
+from discord.ext import commands
+import wavelink
+import subprocess
+import threading
+import multiprocessing
+import atexit
+import logging
+from typing import (
+    Literal,
+    Dict,
+    Union,
+    Optional,
+    List,
+)
+from bot.utils.bot_data import Server
+from bot.utils.music_helpers import Playlist
+
+
+intents = discord.Intents.all()
+# intents.message = True
+# intents.voice_states = True
+# intents.
+
+
+class Kagami(commands.Bot):
+    def __init__(self):
+        with open("bot/data/config.json") as f:
+            self.config = json.load(f)
+
+        super(Kagami, self).__init__(command_prefix=self.config["prefix"],
+                                     intents=intents,
+                                     owner_id=self.config["owner"])
+        self.data = {}
+        self.servers: dict[str, Server] = {}
+        self.global_tags = {}
+
+        self.load_data()
+
+
+
+    def fetch_server(self, server_id: [int, str]):
+        if str(server_id) not in self.servers:
+            self.servers[str(server_id)] = Server(server_id)
+        return self.servers[str(server_id)]
+
+
+    def create_server_list(self):
+        if "servers" not in self.data.keys():
+            # print("no servers")
+            return
+        for server_id, server in self.data["servers"].items():
+            self.servers[server_id] = Server(server_id)
+            server_data_keys = self.data["servers"][server_id].keys()
+
+            if "playlists" in server_data_keys:
+                for playlist_name, playlist_tracks in self.data["servers"][server_id]["playlists"].items():
+                    self.servers[server_id].playlists[playlist_name] = Playlist(playlist_name, playlist_tracks)
+
+            if "soundboard" in server_data_keys:
+                for sound_name, sound_id in self.data["servers"][server_id]["soundboard"].items():
+                    self.servers[server_id].soundboard[sound_name] = sound_id
+
+            if "tags" in server_data_keys:
+                self.servers[server_id].tags = self.data["servers"][server_id]["tags"]
+
+
+
+
+
+
+    def update_data(self):
+        # print(self.data)
+        data: dict[str, dict[str, dict[str, dict[str, str]]]] = {
+            "global_tags": {},
+            "servers": {}
+        }
+
+        data["global_tags"].update(self.global_tags)
+
+        for server_id, server in self.servers.items():
+            data["servers"][server_id] = {"playlists": {},
+                                          "soundboard": {},
+                                          "tags": {}
+                                          }
+            for playlist_name, playlist in server.playlists.items():
+                data["servers"][server_id]["playlists"].update({playlist_name: playlist.tracks})
+
+            for sound_name, sound_id in server.soundboard.items():
+                data["servers"][server_id]["soundboard"].update({sound_name: sound_id})
+
+            data["servers"][server_id]["tags"].update(server.tags.items())
+
+            # for tag_name, tag_data in server_id.tags.items():
+            #     data["servers"][server_id]["tags"].update({tag_name: tag_data})
+
+
+        self.data = data
+
+
+    async def start(self, token, reconnect=True):
+        await super().start(token, reconnect=reconnect)
+
+
+    def start_bot(self):
+        self.run(self.config["token"])
+
+
+    def save_data(self):
+        self.update_data()
+        with open("bot/data/data.json", "w") as f:
+            json.dump(self.data, f, indent=4)
+
+    def load_data(self):
+        with open("bot/data/data.json", "r") as f:
+            self.data = json.load(f)
+
+
+        self.create_server_list()
+        if "global_tags" in self.data.keys():
+            self.global_tags: dict[str, dict] = self.data["global_tags"]
+
+
+    async def close(self):
+        # for node_id, node in wavelink.NodePool.nodes.values():
+        #     await node.disconnect()
+
+        for cog in self.cogs:
+            cog_obj = self.get_cog(cog)
+            await cog_obj.cog_unload()
+
+        self.update_data()
+        self.save_data()
+        print("ran atexit\n")
+        await super().close()
+
+    async def setup_hook(self):
+        for file in os.listdir("bot/cogs"):
+            if file.endswith(".py"):
+                name = file[:-3]
+                await self.load_extension(f"cogs.{name}")
+        # await self.tree.sync()
+
+
+
+
+
+
+    async def on_ready(self):
+        print(f"Logged in as {self.user} (ID: {self.user.id})")
