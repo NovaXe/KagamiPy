73038daa1a285e656ee5f9ca3946c6a95ed0d163
NovaXe <nova8x@proton.me>
2024-10-26 22:06:27 -0400

    Shifted to Simpler Implementation Leaving room for potential changes in the future but for now the implementation will just be custom prefixes per server with role restricitons.

--- kagami/cogs/colors.py
+++ kagami/cogs/colors.py
@@ -87,132 +87,167 @@ from common.utils import acstr
 @dataclass
 class ColorSettings(Table, schema_version=1, trigger_version=1):
     guild_id: int
-    color_prefix: str
-
-
-@dataclass
-class ColorRoles(Table, schema_version=1, trigger_version=1):
-    guild_id: int
-    group_name: int
-    role_id: int=None
-    regex: str=None
+    prefix: str
     
     @classmethod
     async def create_table(cls, db: aiosqlite.Connection):
         query = f"""
-        CREATE TABLE IF NOT EXISTS {ColorRoles}(
+        CREATE TABLE IF NOT EXISTS {ColorSettings}(
             guild_id INTEGER NOT NULL,
-            group_name TEXT NOT NULL,
-            role_id INTEGER,
-            regex TEXT,
-            FOREIGN KEY(guild_id) REFERENCES {Guild}.id
+            prefix TEXT
         )
         """
         await db.execute(query)
-
-
-@dataclass
-class ColorPreview(Table, schema_version=1, trigger_version=1):
-    guild_id: int
-    group_name: str
-    image_data: bytes
-    
-    @classmethod
-    async def create_table(cls, db: aiosqlite.Connection):
-        query = f"""
-        CREATE TABLE IF NOT EXISTS {ColorPreview}(
-            guild_id INTEGER NOT NULL,
-            group_name TEXT NOT NULL,
-            iamge_data BLOB,
-            PRIMARY KEY (guild_id, group_name),
-            FOREIGN KEY (guild_id) REFERENCES {Guild}.id
-        )
-        """
-        await db.execute(query)
-    
-    def getImage(self):
-        pass # use pillow to convert bytes to an image
-    
-
-@dataclass
-class ColorGroup(Table, schema_version=1, trigger_version=1):
-    guild_id: int
-    name: str
-    permitted_role_id: int=0
-    
-    @classmethod
-    async def create_table(cls, db: aiosqlite.Connection):
-        query = f"""
-        CREATE TABLE IF NOT EXISTS {ColorGroup}(
-            guild_id INTEGER NOT NULL,
-            name TEXT NOT NULL,
-            permitted_role_id INTEGER,
-            PRIMARY KEY (guild_id, name),
-            FOREIGN KEY (guild_id) REFERENCES {Guild}.id
-        )
-        """
-
-    @classmethod
-    async def create_triggers(cls, db: aiosqlite.Connection):
-        trigger = f"""
-        CREATE TRIGGER IF NOT EXISTS {ColorGroup}_insert_guild_before_insert
-        BEFORE INSERT ON {ColorGroup}
-        BEGIN
-            INSERT OR IGNORE INTO {Guild}(id)
-            VALUES (NEW.guild_id);
-        END
-        """
-        await db.execute(trigger)
         
-    async def upsert(self, db: aiosqlite.Connection):
-        query = f"""
-        INSERT INTO {ColorGroup}(guild_id, name, permitted_role_id)
-        VALUES(:guild_id, :name, :permitted_role_id)
-        ON CONFLICT (guild_id, name)
-        DO UPDATE SET
-            permitted_role_id = :permitted_role_id
-        RETURNING *
-        """
-        db.row_factory = ColorGroup.row_factory
-        raise NotImplementedError
-
     @classmethod
-    async def selectNamesWheres(cls, db: aiosqlite.Connection, guild_id: int, limit: int=25, offset: int=0) -> list[str]:
+    async def selectPrefixWhere(cls, db: aiosqlite.Connection, guild_id: int) -> str:
         query = f"""
-        SELECT name FROM {ColorGroup} WHERE guild_id = ?
-        LIMIT ? OFFSET ?
+        SELECT prefix FROM {ColorSettings}
+        WHERE guild_id = ?
         """
         db.row_factory = aiosqlite.Row
-        async with db.execute(query, (guild_id, limit, offset)) as cur:
-            res = await cur.fetchall()
-        return [row["name"] for row in res]
+        async with db.execute(query, (guild_id,)) as cur:
+            res = await cur.fetchone()
+        return res["prefix"] if res else None 
     
-    @classmethod
-    async def selectWhere(cls, db: aiosqlite.Connection, guild_id: int, name: str) -> "ColorGroup":
+    async def upsert(self, db: aiosqlite.Connection) -> "ColorSettings":
         query = f"""
-        SELECT * FROM {ColorGroup}
-        WHERE
-            guild_id = ?,
-            name = ?
+        INSERT INTO {ColorSettings}(guild_id, prefix)
+        VALUES (:guild_id, :prefix)
+        ON CONFLICT (guild_id)
+        DO UPDATE SET
+            prefix = :prefix
+        RETURNING *
         """
-        db.row_factory = ColorGroup.row_factory
-        async with db.execute(query, (guild_id, name)) as cur:
+        db.row_factory = ColorSettings.row_factory
+        async with db.execute(query, self.asdict()) as cur:
             res = await cur.fetchone()
         return res
 
 
-class GroupTransformer(Transformer):
-    async def autocomplete(self, interaction: Interaction[Kagami], value: str) -> List[Choice[str]]:
-        async with interaction.client.dbman.conn() as db:
-            groups = await ColorGroup.selectNamesWhere(db, interaction.guild_id)
-        return [Choice(g, g) for g in groups]
+# @dataclass
+# class ColorRoles(Table, schema_version=1, trigger_version=1):
+#     guild_id: int
+#     group_name: int
+#     role_id: int=None
+#     regex: str=None
     
-    async def transform(self, interaction: Interaction[Kagami], value: str) -> str:
-        async with interaction.client.dbman.conn() as db:
-            group = await ColorGroup.selectWhere(db, interaction.guild_id, value)
-        return group
+#     @classmethod
+#     async def create_table(cls, db: aiosqlite.Connection):
+#         query = f"""
+#         CREATE TABLE IF NOT EXISTS {ColorRoles}(
+#             guild_id INTEGER NOT NULL,
+#             group_name TEXT NOT NULL,
+#             role_id INTEGER,
+#             regex TEXT,
+#             FOREIGN KEY(guild_id) REFERENCES {Guild}.id
+#         )
+#         """
+#         await db.execute(query)
 
-Group_Transform = Transform[ColorGroup, GroupTransformer]
+
+# @dataclass
+# class ColorPreview(Table, schema_version=1, trigger_version=1):
+#     guild_id: int
+#     group_name: str
+#     image_data: bytes
+    
+#     @classmethod
+#     async def create_table(cls, db: aiosqlite.Connection):
+#         query = f"""
+#         CREATE TABLE IF NOT EXISTS {ColorPreview}(
+#             guild_id INTEGER NOT NULL,
+#             group_name TEXT NOT NULL,
+#             iamge_data BLOB,
+#             PRIMARY KEY (guild_id, group_name),
+#             FOREIGN KEY (guild_id) REFERENCES {Guild}.id
+#         )
+#         """
+#         await db.execute(query)
+    
+#     def getImage(self):
+#         pass # use pillow to convert bytes to an image
+    
+
+# @dataclass
+# class ColorGroup(Table, schema_version=1, trigger_version=1):
+#     guild_id: int
+#     name: str
+#     permitted_role_id: int=0
+    
+#     @classmethod
+#     async def create_table(cls, db: aiosqlite.Connection):
+#         query = f"""
+#         CREATE TABLE IF NOT EXISTS {ColorGroup}(
+#             guild_id INTEGER NOT NULL,
+#             name TEXT NOT NULL,
+#             permitted_role_id INTEGER,
+#             PRIMARY KEY (guild_id, name),
+#             FOREIGN KEY (guild_id) REFERENCES {Guild}.id
+#         )
+#         """
+
+#     @classmethod
+#     async def create_triggers(cls, db: aiosqlite.Connection):
+#         trigger = f"""
+#         CREATE TRIGGER IF NOT EXISTS {ColorGroup}_insert_guild_before_insert
+#         BEFORE INSERT ON {ColorGroup}
+#         BEGIN
+#             INSERT OR IGNORE INTO {Guild}(id)
+#             VALUES (NEW.guild_id);
+#         END
+#         """
+#         await db.execute(trigger)
+        
+#     async def upsert(self, db: aiosqlite.Connection):
+#         query = f"""
+#         INSERT INTO {ColorGroup}(guild_id, name, permitted_role_id)
+#         VALUES(:guild_id, :name, :permitted_role_id)
+#         ON CONFLICT (guild_id, name)
+#         DO UPDATE SET
+#             permitted_role_id = :permitted_role_id
+#         RETURNING *
+#         """
+#         db.row_factory = ColorGroup.row_factory
+#         raise NotImplementedError
+
+#     @classmethod
+#     async def selectNamesWheres(cls, db: aiosqlite.Connection, guild_id: int, limit: int=25, offset: int=0) -> list[str]:
+#         query = f"""
+#         SELECT name FROM {ColorGroup} WHERE guild_id = ?
+#         LIMIT ? OFFSET ?
+#         """
+#         db.row_factory = aiosqlite.Row
+#         async with db.execute(query, (guild_id, limit, offset)) as cur:
+#             res = await cur.fetchall()
+#         return [row["name"] for row in res]
+    
+#     @classmethod
+#     async def selectWhere(cls, db: aiosqlite.Connection, guild_id: int, name: str) -> "ColorGroup":
+#         query = f"""
+#         SELECT * FROM {ColorGroup}
+#         WHERE
+#             guild_id = ?,
+#             name = ?
+#         """
+#         db.row_factory = ColorGroup.row_factory
+#         async with db.execute(query, (guild_id, name)) as cur:
+#             res = await cur.fetchone()
+#         return res
+
+
+# class GroupTransformer(Transformer):
+#     async def autocomplete(self, interaction: Interaction[Kagami], value: str) -> List[Choice[str]]:
+#         async with interaction.client.dbman.conn() as db:
+#             groups = await ColorGroup.selectNamesWhere(db, interaction.guild_id)
+#         return [Choice(g, g) for g in groups]
+    
+#     async def transform(self, interaction: Interaction[Kagami], value: str) -> str:
+#         async with interaction.client.dbman.conn() as db:
+#             group = await ColorGroup.selectWhere(db, interaction.guild_id, value)
+#         return group
+
+# Group_Transform = Transform[ColorGroup, GroupTransformer]
 
 @app_commands.default_permissions(manage_roles=True)
 class ColorCogAdmin(GroupCog, name="color"):
@@ -221,11 +256,11 @@ class ColorCogAdmin(GroupCog, name="color"):
         self.config = bot.config
         self.dbman = bot.dbman
     
-    @app_commands.command(name="register", description="registers a new arbitrary color role")
-    async def register(self, interation: Interaction, role: discord.Role, group: Group_Transform="everyone"):
-        await respond(interation, ephemeral=True)
-        async with self.bot.dbman.conn() as db:
-            raise NotImplementedError
+    # @app_commands.command(name="register", description="registers a new arbitrary color role")
+    # async def register(self, interation: Interaction, role: discord.Role, group: Group_Transform="everyone"):
+    #     await respond(interation, ephemeral=True)
+    #     async with self.bot.dbman.conn() as db:
+    #         raise NotImplementedError
 
 
 class ColorCog(GroupCog, name="color"):
@@ -246,6 +281,12 @@ class ColorCog(GroupCog, name="color"):
     
     async def on_ready(self):
         pass
+    
+    @app_commands.command(name="preview", description="Generates an image preview of all colors on the server")
+    async def preview(self, interaction: Interaction):
+        await respond(interaction)
+        raise NotImplementedError
+
     
     
 
